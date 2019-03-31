1914659746d2c5414df3b89b2fc19de1c8d0054f
Extibax <extibax@gmail.com>
2019-03-31 15:06:35 -0500

    Update March

---
+++ js/App.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    var scrollToElement = function (el, ms) {
+        var speed = (ms) ? ms : 600;
+        $('html,body').animate({
+            scrollTop: $(el).offset().top
+        }, speed);
+    }
+
+    function aboutMe() {
+        /* window.location.hash = '#aboutMe'; */
+        /* document.getElementById('aboutMe').scrollIntoView(); */
+        scrollToElement('#aboutMe', 600);
+    }
+
+    function works() {
+        /* window.location.hash = '#works';
+        document.getElementById('works').scrollIntoView(); */
+        scrollToElement('#works', 600);
+    }
+
+    function contact() {
+        /* window.location.hash = '#contact';
+        document.getElementById('contact').scrollIntoView(); */
+        scrollToElement('#contact', 600);
+    }
+
+    function copyClipboard() {
+        var email = "extibax@gmail.com";
+        navigator.clipboard.writeText(email);
+        alertify.success('¡The email has been copied!');
+    }
+
+    $.get('https://api.github.com/users/Extibax', 'aplication/json', function (response) {
+        let extibax = {
+            name: response.name,
+            bio: response.bio
+        }
+
+        let kn_list = extibax.bio.split(':', 2), kn_clear = kn_list[1].split('\r\n'), 
+        knowledges = kn_clear[1].split(','), template = '', index = 0;
+
+        knowledges.forEach(function (knowledge) {
+            template +=`<span>${$.trim(knowledge)}</span><br>`;
+        });
+
+        $('#knowledge_list').html(template);
+    })
+});
