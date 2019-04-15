b96412a872167ec47801f750a54a765477847fb6
Extibax <extibax@gmail.com>
2019-04-15 13:05:25 -0500

    Nav items fixed

--- js/App.js
+++ js/App.js
@@ -1,34 +1,29 @@
+var scrollToElement = function (el, ms) {
+    var speed = (ms) ? ms : 600;
+    $('html,body').animate({
+        scrollTop: $(el).offset().top
+    }, speed);
+}
+
+function about() {
+    scrollToElement('#about', 600);
+}
+
+function works() {
+    scrollToElement('#works', 600);
+}
+
+function contact() {
+    scrollToElement('#contact', 600);
+}
+
+function copyClipboard() {
+    var email = "extibax@gmail.com";
+    navigator.clipboard.writeText(email);
+    alertify.success('¡The email has been copied!');
+}
+
 $(document).ready(function () {
-    var scrollToElement = function (el, ms) {
-        var speed = (ms) ? ms : 600;
-        $('html,body').animate({
-            scrollTop: $(el).offset().top
-        }, speed);
-    }
-
-    function aboutMe() {
-        /* window.location.hash = '#aboutMe'; */
-        /* document.getElementById('aboutMe').scrollIntoView(); */
-        scrollToElement('#aboutMe', 600);
-    }
-
-    function works() {
-        /* window.location.hash = '#works';
-        document.getElementById('works').scrollIntoView(); */
-        scrollToElement('#works', 600);
-    }
-
-    function contact() {
-        /* window.location.hash = '#contact';
-        document.getElementById('contact').scrollIntoView(); */
-        scrollToElement('#contact', 600);
-    }
-
-    function copyClipboard() {
-        var email = "extibax@gmail.com";
-        navigator.clipboard.writeText(email);
-        alertify.success('¡The email has been copied!');
-    }
 
     $.get('https://api.github.com/users/Extibax', 'aplication/json', function (response) {
         let extibax = {
@@ -36,11 +31,11 @@ $(document).ready(function () {
             bio: response.bio
         }
 
-        let kn_list = extibax.bio.split(':', 2), kn_clear = kn_list[1].split('\r\n'), 
-        knowledges = kn_clear[1].split(','), template = '';
+        let kn_list = extibax.bio.split(':', 2), kn_clear = kn_list[1].split('\r\n'),
+            knowledges = kn_clear[1].split(','), template = '';
 
         knowledges.forEach(function (knowledge) {
-            template +=`<span>${$.trim(knowledge)}</span><br>`;
+            template += `<span>${$.trim(knowledge)}</span><br>`;
         });
 
         $('#knowledge_list').html(template);
